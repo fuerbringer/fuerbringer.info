f11e940cfd40eef82e505787b4b5a2c52a5f9fa5
fuerbringer <severin@protonmail.ch>
2017-07-20 22:56:42 +0200

    Added allow robots flag to toggle meta tag.

--- config/site-config.js
+++ config/site-config.js
@@ -5,8 +5,11 @@ module.exports = {
   name: 'Personal Site by Severin Fürbringer',
   brand: 'Severin Fürbringer',
   titleSeparator: ' | ',
+  allowRobots: true,
+
   licenseLegal: 'https://creativecommons.org/licenses/by-sa/4.0/legalcode',
   license: 'https://creativecommons.org/licenses/by-sa/4.0/',
+
   pages: {
     about: {
       title: 'About'

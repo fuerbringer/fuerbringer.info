6156f9f308cd8afc6a5182cbfdf99842091414f2
fuerbringer <severin@protonmail.ch>
2017-07-22 20:21:02 +0200

    Added LibreJS license headers to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3.0
 String.prototype.scramble = function(){
   var that = this.split(" ");
   var len = that.length;
@@ -37,3 +38,4 @@ $(document).ready(function() {
   glitchTagsOnHover(bodyLinks);
   glitchTagsOnHover(footerLinks);
 });
+// @license-end

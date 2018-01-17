c81ff8ba65697a329ed0246fcc64f76b2cf550c7
fuerbringer <severin@protonmail.ch>
2018-01-17 23:23:16 +0100

    Improve glitch link hover effect while making site faster.

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,5 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
-String.prototype.scramble = function(){
-	var that = this.split(" ");
-	var len = that.length;
+// No custom JS needed... for now.
 
-	for(var i = 0; i < len; i++) {
-		var word = that[i];
-		for(var y = 1; y < word.length - 1; y++) {
-			var charCode = word.charCodeAt(y);
-			var delta = (Math.floor(Math.random() * 4) - 2);
-			var charGlitched = String.fromCharCode(charCode + delta);
-			word = word.replace(word[y], charGlitched);
-		}
-		that[i] = word;
-	}
-
-	return that.join("");
-}
-
-// Add hover glitch effect on links:
-function glitchTagsOnHover(tag) {
-	$(tag).each(function() {
-		var aTextShuffled = $(this).text().scramble();
-
-		$(this).hover(function (e) {
-			$(this).attr('data-text', aTextShuffled);
-			$(this).addClass('glitch gan');
-		}, function() {
-			$(this).removeAttr('data-text');
-			$(this).removeClass('glitch gan');
-		});
-	});
-}
-
-function glitchTags(tag) {
-	$(tag).each(function() {
-		var aTextShuffled = $(this).text().scramble();
-		$(this).attr('data-text', aTextShuffled);
-		$(this).addClass('glitch gan');
-	});
-}
-
-$(document).ready(function() {
-	var headerText = '#navbar-brand-text';
-	var bodyLinks = '#body a';
-	var footerLinks = '#footer a';
-
-	glitchTags(headerText);
-	glitchTagsOnHover(bodyLinks);
-	glitchTagsOnHover(footerLinks);
-});
 // @license-end
